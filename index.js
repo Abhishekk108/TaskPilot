0abda909a2303687eb66b39355fce3cccba36df7
Abhishek Kallimath <abhikalimath@gmail.com>
2026-08-13 17:03:14 +0530

    Implement CRUD functions for managing todos in the database

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 import {db} from './db/index.js';
 import {todosTable} from './db/schema.js';
+import { ilike, eq } from 'drizzle-orm';
+
+async function getAllTodos(){
+    const todos = await db.select().from(todosTable);
+    return todos;
+}
+async function createTodo(todo){
+    const newTodo = await db.insert(todosTable).values({todo});
+    return newTodo;
+}
+async function searchTodos(search){
+    const todos = await db.select().from(todosTable).where(ilike(todosTable.todo, search));
+    return todos;
+}
+
+async function deleteTodo(id){
+    const deletedTodo = await db.delete(todosTable).where(todosTable.id.eq(id));
+    return deletedTodo;
+}
